0669cea74b2caf09d9c9bca968ee29b4586d9129
Keita Tokunaga <ketaketa1985@gmail.com>
2023-01-15 23:49:44 +0900

    実装検証用の起動パラメタクラスを追加 以下の調査コードを追加 * クラスメソッドに、decoratorを実装 * decoratorでクラス変数を生成 * decoratorでargparserを利用

--- bin/A002_decorator/sample_decorator.py
+++ bin/A002_decorator/sample_decorator.py
@@ -7,18 +7,33 @@
 
 import functools
 from typing import Callable, TypeVar
-from argparse import FileType
+from argparse import FileType, ArgumentParser
 
 _T = TypeVar("_T")
 
 class DecoSample():
 
+    parser = ArgumentParser()
+
+    @classmethod
+    def get_arg_name(name: str, is_option: bool) -> str:
+        return name if is_option else f'--{name}'
+    
+    @classmethod
     def sample_deco(
         type: Callable[[str], _T] | FileType,
-        help: str, 
-        validator: Callable=None
-    ):
+        help: str,
+        is_option: bool=True 
+    ):  
+
         def _sample_deco(func):
+
+            # パラメタの追加
+            DecoSample.parser.add_argument(
+                DecoSample.get_arg_name(func.__name__), 
+                type=type, 
+                help=help
+            )
 
             @functools.wraps(func)
             def _wrapper(*args, **keywords):
@@ -34,3 +49,4 @@ class DecoSample():
             return _wrapper
 
         return _sample_deco
+
