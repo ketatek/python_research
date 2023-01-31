cdedbef9d5d676ac03214875890731d34ecc2591
Keita Tokunaga <ketaketa1985@gmail.com>
2023-01-31 21:30:50 +0900

    ロガーの検証コード追加

--- bin/A004_logger/main_02.py
+++ bin/A004_logger/main_02.py
@@ -2,11 +2,18 @@ import research_log_02
 
 def main():
 
-    logger_001 = research_log_02.create_logger_101()
-    logger_001.debug("debug")
-    logger_001.info("info")
-    logger_001.error("erro")
-    logger_001.warning("warning")
+    # logger_001 = research_log_02.create_logger_101()
+    # logger_001.debug("debug")
+    # logger_001.info("info")
+    # logger_001.error("erro")
+    # logger_001.warning("warning")
+
+    logger_002 = research_log_02.create_logger_102()
+    logger_002.debug("debug")
+    logger_002.info("info")
+    logger_002.error("error")
+    logger_002.warning("warning")
+
 
 if __name__ == '__main__':
     main()
--- bin/A004_logger/research_log_02.py
+++ bin/A004_logger/research_log_02.py
@@ -39,23 +39,10 @@ def create_logger_101():
 
 def create_logger_102():
 
-    logger = getLogger(f"{__name__}_101")
-    logger.setLevel(DEBUG)
-
-    formatter = Formatter(  
-        fmt='%(asctime)s > [%(levelname)s] %(module)s > %(message)s'
-    )
-
-    h01 = StreamHandler(sys.stderr)
-    h01.setLevel(INFO)
-    h01.setFormatter(formatter)
+    with open('logging_02.json', 'rt') as f:
+        config = json.load(f)
+        logging.config.dictConfig(config)
     
-
-    h02 = FileHandler("rlog_02_101.log")
-    h02.setLevel(DEBUG)
-    h02.setFormatter(formatter)
-
-    logger.addHandler(h01)
-    logger.addHandler(h02)
+    logger = getLogger("research_log_02")
 
     return logger
